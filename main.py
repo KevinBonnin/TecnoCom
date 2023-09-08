ae0e2f2815c1c3c977526e27a94f8a1a468cb809
KevinBonnin <kevin.bonnin65@gmail.com>
2023-09-08 15:50:55 -0300

    7

--- main.py
+++ main.py
@@ -77,3 +77,48 @@ def juegos_recomendados(juego: str):
     #recommended_titles = recommended_games['title'].head(5).tolist()
 
     return recommended_games[['title']].head(5)
+
+@app.get("/recomendacion2/{juego}/Recomienda 4 juegos similares")
+def juegos_recomendados(juego: str):
+    # Obtener las características de los juegos
+    generos = inf['genres'].fillna('').tolist()
+    aspectos = inf['specs'].fillna('').tolist()
+    etiquetas = inf['tags'].fillna('').tolist()
+
+    n_components = min(50, len(etiquetas[0].split('|')))
+
+    # Vectorizar las etiquetas de los juegos utilizando TF-IDF
+    vectorizer_etiquetas = TfidfVectorizer()
+    etiquetas_vectors = vectorizer_etiquetas.fit_transform(etiquetas)
+    # Reducción de dimensionalidad con LSA
+    lsa_model_etiquetas = TruncatedSVD(n_components=n_components)
+    etiquetas_vectors_reduced = lsa_model_etiquetas.fit_transform(etiquetas_vectors)
+
+    vectorizer_aspectos = TfidfVectorizer()
+    aspectos_vectors = vectorizer_aspectos.fit_transform(aspectos)
+    lsa_model_aspectos = TruncatedSVD(n_components=n_components)
+    aspectos_vectors_reduced = lsa_model_aspectos.fit_transform(aspectos_vectors)
+
+    vectorizer_generos = TfidfVectorizer()
+    generos_vectors = vectorizer_generos.fit_transform(generos)
+    lsa_model_generos = TruncatedSVD(n_components=n_components)
+    generos_vectors_reduced = lsa_model_generos.fit_transform(generos_vectors)
+    
+    feature_vectors = np.concatenate((etiquetas_vectors_reduced, aspectos_vectors_reduced, generos_vectors_reduced), axis=1)
+    column_names = ['Feature_' + str(i+1) for i in range(3 * n_components)]
+    df_feature_vectors = pd.DataFrame(data=feature_vectors, columns=column_names)
+    feature_vectors = df_feature_vectors.values
+
+    n_neighbors = 6
+    metric = 'cosine'
+    model = NearestNeighbors(n_neighbors=n_neighbors, metric=metric)
+    model.fit(feature_vectors)
+
+    game_index = inf[inf['title'] == juego].index[0]
+
+    s, indices = model.kneighbors(feature_vectors[game_index].reshape(1, -1))
+
+    recommended_games = inf.loc[indices.flatten()].copy()
+    #recommended_titles = recommended_games['title'].head(5).tolist()
+
+    return recommended_games[['title']].head(5)
